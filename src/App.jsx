577e14a3160dc4020fbcd01ce6a1eec045522342
gudwin <gudwinnayak786@gmail.com>
2023-12-31 14:17:54 +0530

    getCourse

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 import React from 'react'
 
+export async function getCourses() {
+  try {
+    const url = `https://www.udemy.com/api-2.0/course/`;
+    const headers = {
+      "Authorization": "Basic Xj925OKZeT41Fp0VDVjarABMgAbYXdWsoZ5XrE406oDeinEH3RDkE35eicieQqHbOfNFgVu0i7D2ksfi3VmsXHxQuJpKfZ1rpxiASYobKzKSFxTKc2CPT5YD51eq60cX" ,
+      "Content-Type": "application/json",
+    };
+    const params = new URLSearchParams({
+      'page_size': 20,
+      'page':200
+    });
+    const response = await fetch(`${url}?${params}&fields[course]=@all`, {
+      headers: headers,
+    });
+    if (!response.ok) {
+      return []
+    } else {
+      const result = await response.json();
+      return result;
+  }
+  } catch (error) {
+    console.log(error)
+  }
+}
+getCourses()
+
 const App = () => {
   return (
     <div>Hello world</div>
